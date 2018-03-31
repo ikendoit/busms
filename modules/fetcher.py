4cb7111612b19cd09e442f3b2b901c4da563c892
Ken Nguyen <ikendoit1998@gmail.com>
2018-03-30 18:25:00 -0700

    v0.5: sms service ON

--- modules/fetcher.py
+++ modules/fetcher.py
@@ -1,7 +1,7 @@
 import requests 
 
 def fetch(stop_id):
-    data = requests.get("http://api.translink.ca/rttiapi/v1/stops/"+stop_id+"/estimates?apikey=MMKpRVbHI434lbDjTwZi");
+    data = requests.get("http://api.translink.ca/rttiapi/v1/stops/"+stop_id+"/estimates?apikey=YOUR_TRANSLINK_API_KEY");
 
     return data.text
 
