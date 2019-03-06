980c6099cdbac343890eb49da8e725ce55d56ea3
root <root@buntu-server.c.snappy-spanner-196507.internal>
2019-03-06 07:41:27 +0000

    added end point to support author's work + dataset

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
 import urllib
 
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify, send_from_directory
 from twilio.twiml.messaging_response import Message, MessagingResponse
+from flask_cors import CORS
+from datetime import datetime
+import json
 
 from modules.Runner import runner, runner_api
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build/')
+
+CORS(app);
 
 @app.route("/", methods=["GET"])
 def index():
@@ -27,8 +32,6 @@ def api_bus():
     data = runner_api(query)
     return jsonify(data)
 
-
-
 @app.route("/sms", methods=["POST"])
 def sms():
     number = request.form["From"] 
@@ -43,3 +46,31 @@ def sms():
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
+@app.route("/logger", methods=["GET"])
+def api_logger():
+    # add 7 hours to see viet nam
+    with open("ips.txt", "a") as f: 
+        current = {};
+        current["ip"]=request.remote_addr
+        current["time"]=str(datetime.utcnow())
+        current["back"]=str(request.args.get("ip"))
+        current["page"]=str(request.args.get("ip1"))
+        current["platform"]=str(request.args.get("platform"))
+        current["agent"]=str(request.args.get("agent"))
+        f.write(","+json.dumps(current));
+    return jsonify({});
+
+@app.route("/samsonhotel", methods=["GET"])
+def api_samsonhotel():
+    data = "";
+    with open("ips.txt", "r") as f: 
+        data=f.read();
+    return "["+data+"]";
+
+@app.route('/samson',methods=["GET"])
+def samson():
+    return send_from_directory('build/','index.html')
+
+@app.route('/static/<path:path>',methods=["GET"])
+def static_serve(path=''):
+    return send_from_directory('build/static/',path)
--- modules/fetcher.py
+++ modules/fetcher.py
@@ -1,7 +1,7 @@
 import requests 
 
 def fetch(stop_id):
-    data = requests.get("http://api.translink.ca/rttiapi/v1/stops/"+stop_id+"/estimates?apikey=YOUR_TRANSLINK_KEY");
+    data = requests.get("http://api.translink.ca/rttiapi/v1/stops/"+stop_id+"/estimates?apikey=<auth_key>");
 
     return data.text
 
